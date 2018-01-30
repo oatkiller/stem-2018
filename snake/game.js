dbfdb224a2fb90be5716d3e84c3e5a96e535ce26
oatkiller <raustin@endgame.com>
2018-01-30 08:30:15 -0500

    better snakes

--- snake/game.js
+++ snake/game.js
@@ -1,50 +1,71 @@
+// How long to wait between procesing a turn (in milliseconds)
+const gameSpeed = 1000 / 30
+
+// Width / height of a tile in pixels
+const tileSize = 10
+
+// The width and height of the movable area
+const gridWidth = 60
+const gridHeight = 60
+
+// represents player two in the board data
 const playerOne = {
-  xVelocity: 0,
-  yVelocity: -1,
+  // starting direction
+  direction: 'down',
+
+  // starting position
   x: 20,
   y: 30,
+
+  // snake color
   fillStyle: 'red',
-  name: 'one',
+
+  // used in winning message
+  name: 'Red',
 }
 
+// represents player two in the board data
 const playerTwo = {
-  xVelocity: 0,
-  yVelocity: 1,
+  // starting direction
+  direction: 'up',
+
+  // starting position
   x: 40,
   y: 30,
+
+  // snake color
   fillStyle: 'blue',
-  name: 'two',
+
+  // used in winning message
+  name: 'Blue',
 }
 
+// represents a wall in the board data
 const wall = {
   fillStyle: 'black',
 }
 
-const tileSize = 10
-const gridWidth = 60
-const gridHeight = 60
-
 // Empty board
-const board = []
+const board = initializedBoard()
 
-// initialize board and add walls
-for (let x = 0; x < gridWidth; x++) {
+// The players take turns moving, that way there can never be a tie
+// The turn counter determines which player is moving
+// playerOne plays on odd turn numbers
+// playerTwo plays on even turn numbers
+let turn = 0
 
-  // create a row
-  board[x] = []
-  board[x].length = gridHeight
+// Add an event listener that will call a function when players press any key
+window.addEventListener(
+  'keydown',
+  handleKeydownEvent,
+  false
+)
 
-  for (let y = 0; y < gridHeight; y++) {
-    // Add walls to the edges
-    if (x === 0 || x === gridWidth - 1 || y === 0 || y === gridHeight - 1) {
-      board[x][y] = wall
-    }
-  }
-}
-
-// place the players
-board[playerOne.x][playerOne.y] = playerOne
-board[playerTwo.x][playerTwo.y] = playerTwo
+// Run `gameSpeed` on an interval, moving the snakes and determining if there is a loser
+const tickInterval = setInterval(
+  tick,
+  gameSpeed
+)
 
 function render() {
   const canvas = document.getElementById('game')
@@ -64,60 +85,112 @@ function render() {
   }
 }
 
+function initializedBoard() {
 
-let turn = 0
-const tick = setInterval(
-  function() {
-    turn = turn + 1
-    if (turn % 2 === 0) {
-      movePlayer(playerOne)
-    } else {
-      movePlayer(playerTwo)
-    }
-    render()
+  // The board is an array of arrays.
+  // Each array represents a column of tiles
+  // Create the initial array (which will hold columns)
+  const board = []
 
-    function movePlayer(player) {
-      player.x += player.xVelocity
-      player.y += player.yVelocity
-      if (board[player.x][player.y]) {
-        alert(player.name + ' loses')
-        clearInterval(tick);
+  // initialize board and add walls
+  for (let x = 0; x < gridWidth; x++) {
+
+    // create a column for each horizontal position
+    board[x] = []
+
+    // Make the column as tall as needed
+    board[x].length = gridHeight
+
+    for (let y = 0; y < gridHeight; y++) {
+      // Add walls to the edges
+      if (x === 0 || x === gridWidth - 1 || y === 0 || y === gridHeight - 1) {
+        board[x][y] = wall
       }
-      board[player.x][player.y] = player
     }
-  },
-  1000 / 30,
-)
+  }
 
-window.addEventListener(
-  'keydown',
-  function(event) {
-    if (event.key === 'w') {
-      playerOne.xVelocity = 0
-      playerOne.yVelocity = -1
-    } else if (event.key === 'd') {
-      playerOne.xVelocity = 1
-      playerOne.yVelocity = 0
-    } else if (event.key === 's') {
-      playerOne.xVelocity = 0
-      playerOne.yVelocity = 1
-    } else if (event.key === 'a') {
-      playerOne.xVelocity = -1
-      playerOne.yVelocity = 0
-    } else if (event.key === 'ArrowUp') {
-      playerTwo.xVelocity = 0
-      playerTwo.yVelocity = -1
-    } else if (event.key === 'ArrowRight') {
-      playerTwo.xVelocity = 1
-      playerTwo.yVelocity = 0
-    } else if (event.key === 'ArrowDown') {
-      playerTwo.xVelocity = 0
-      playerTwo.yVelocity = 1
-    } else if (event.key === 'ArrowLeft') {
-      playerTwo.xVelocity = -1
-      playerTwo.yVelocity = 0
+  // place the players
+  board[playerOne.x][playerOne.y] = playerOne
+  board[playerTwo.x][playerTwo.y] = playerTwo
+
+  return board
+}
+
+function tick() {
+  turn = turn + 1
+  if (turn % 2 === 0) {
+    movePlayer(playerOne)
+  } else {
+    movePlayer(playerTwo)
+  }
+  render()
+
+  function movePlayer(player) {
+    let xVelocity = 0
+    let yVelocity = 0
+
+    if (player.direction === 'up') {
+      yVelocity = -1
+    } else if (player.direction === 'right') {
+      xVelocity = 1
+    } else if (player.direction === 'down') {
+      yVelocity = 1
+    } else if (player.direction === 'left') {
+      xVelocity = -1
+    } else {
+      throw new Error('player direction is goofy')
     }
-    console.log(playerOne, playerTwo)
-  },
-  false
-)
+
+    // Record player location for next time
+    player.x += xVelocity
+    player.y += yVelocity
+
+    // Check if player lost
+    if (board[player.x][player.y]) {
+      alert(player.name + ' loses')
+      // Stop processing the game
+      clearInterval(tickInterval);
+    }
+
+    // Update player location on board
+    board[player.x][player.y] = player
+  }
+}
+
+// When a keyboard key is pressed, handle changing player directions
+function handleKeydownEvent(event) {
+  if (event.key === 'w') {
+    // Prevent player from moving directly back onto themselves
+    if (playerOne.direction !== 'down') {
+      playerOne.direction = 'up'
+    }
+  } else if (event.key === 'd') {
+    if (playerOne.direction !== 'left') {
+      playerOne.direction = 'right'
+    }
+  } else if (event.key === 's') {
+    if (playerOne.direction !== 'up') {
+      playerOne.direction = 'down'
+    }
+  } else if (event.key === 'a') {
+    if (playerOne.direction !== 'right') {
+      playerOne.direction = 'left'
+    }
+  } else if (event.key === 'ArrowUp') {
+    if (playerTwo.direction !== 'down') {
+      playerTwo.direction = 'up'
+    }
+  } else if (event.key === 'ArrowRight') {
+    if (playerTwo.direction !== 'left') {
+      playerTwo.direction = 'right'
+    }
+  } else if (event.key === 'ArrowDown') {
+    if (playerTwo.direction !== 'up') {
+      playerTwo.direction = 'down'
+    }
+  } else if (event.key === 'ArrowLeft') {
+    if (playerTwo.direction !== 'right') {
+      playerTwo.direction = 'left'
+    }
+  }
+}
